d14b4a3927ab517874cca3209ea0054395047f04
SK sanoyar ali <asksanoyar33@gmail.com>
2025-10-09 01:08:12 +0530

    added clerk and inngest configarations

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,24 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from 'inngest/express'
+import { inngest, functions } from './inngest/index.js'
 import connectDb from './configs/db.js'
 dotenv.config()
 const app = express()
 const port = 3000
-app.use(express.json())
-app.use(cors())
 connectDb()
+//middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+app.use(clerkMiddleware())
+
 app.get('/', (req, res) => {
   res.send('server is live')
 })
+app.use('/api/inngest', serve({ client: inngest, functions }))
 
 app.listen(port, () => {
   console.log(`Server is listening at port ${port}`)
---
+++ backend/inngest/index.js
@@ -0,0 +1,5 @@
+import { Inngest } from 'inngest'
+export const inngest = new Inngest({ id: 'movie-ticket-booking-app' })
+// inngest functions to save user data to adatbase
+
+export const functions = []
