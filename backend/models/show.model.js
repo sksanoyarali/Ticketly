060bcd56595a93d563f0100c884bf8d32ab937c6
SK sanoyar ali <asksanoyar33@gmail.com>
2025-10-09 20:34:57 +0530

    fix:fixed typo at show model

--- backend/models/show.model.js
+++ backend/models/show.model.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 const showSchema = new mongoose.Schema(
   {
     movie: { type: String, required: true, ref: 'Movie' },
-    showDatetime: { type: Date, required: true },
+    showDateTime: { type: Date, required: true },
     showPrice: { type: Number, required: true },
     occupiedSeats: { type: Object, default: {} },
   },
