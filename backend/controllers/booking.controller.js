c2cb8ed9b52ed5b5fa3e1ab88166cc853051b624
SK sanoyar ali <asksanoyar33@gmail.com>
2025-10-10 20:07:53 +0530

    added create booking controller

--- backend/controllers/booking.controller.js
+++ backend/controllers/booking.controller.js
@@ -1,5 +1,6 @@
 // function to check availability of selected seats for a show
 
+import Booking from '../models/booking.model.js'
 import Show from '../models/show.model.js'
 
 const checkSeatsAvailability = async (showId, selectedSeats) => {
@@ -18,5 +19,46 @@ const checkSeatsAvailability = async (showId, selectedSeats) => {
   } catch (error) {
     console.error(error)
     return false
+  }
+}
+const createBooking = async (req, res) => {
+  try {
+    const { userId } = req.auth()
+    const { showId, selectedSeats } = req.body
+    const { origin } = req.headers
+    const isAvailable = await checkSeatsAvailability(showId, selectedSeats)
+    if (!isAvailable) {
+      return res.status(400).json({
+        success: false,
+        message: 'Selected Seats are not available',
+      })
+    }
+    const showData = await Show.findById(showId).populate('movie')
+    // create a new booking
+    const booking = await Booking.create({
+      user: userId,
+      show: showId,
+      amount: showData.showPrice * selectedSeats.length,
+      bookedSeats: selectedSeats,
+    })
+
+    selectedSeats.map((seat) => {
+      showData.occupiedSeats[seat] = userId
+    })
+
+    showData.markModified('occupiedSeats')
+    await showData.save()
+    // stripe Gateway Initialize
+
+    res.status(201).json({
+      success: true,
+      message: 'booked successfully',
+    })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    })
   }
 }
