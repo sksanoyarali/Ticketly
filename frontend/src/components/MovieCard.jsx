2e93f07641c011c8ef652ee36fd71ace15305ee8
SK sanoyar ali <asksanoyar33@gmail.com>
2025-10-13 20:06:52 +0530

    added featured section ui with moviecards

---
+++ frontend/src/components/MovieCard.jsx
@@ -0,0 +1,51 @@
+import { StarIcon } from 'lucide-react'
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { timeFormat } from '../lib/timeFormat'
+
+const MovieCard = ({ movie }) => {
+  const navigate = useNavigate()
+  return (
+    <div
+      className="flex flex-col justify-between p-3 bg-gray-800
+    rounded-2xl hover:-translate-1 transition duration-300 w-66"
+    >
+      <img
+        src={movie.backdrop_path}
+        alt=""
+        className="rounded-lg h-52 w-full object-cover object-right-bottom cursor-pointer"
+        onClick={() => {
+          navigate(`/movies/${movie._id}`)
+          scrollTo(0, 0)
+        }}
+      />
+      <p className="font-semibold mt-2 truncate">{movie.Title}</p>
+      <p className="text-sm text-gray-400 mt-2">
+        {new Date(movie.release_date).getFullYear()} •
+        {movie.genres
+          .slice(0, 2)
+          .map((genre) => genre.name)
+          .join(' | ')}{' '}
+        • {timeFormat(movie.runtime)}
+      </p>
+      <div className="flex items-center justify-between mt-4 pb-3">
+        <button
+          className="px-4 py-2 text-xs bg-primary hover:bg-primary-dull
+        transition rounded-full font-medium cursor-pointer"
+          onClick={() => {
+            navigate(`/movies/${movie._id}`)
+            scrollTo(0, 0)
+          }}
+        >
+          Buy Tickets
+        </button>
+        <p className="flex flex-center gap-1 text-sm text-gray-400 mt-1 pr-1">
+          <StarIcon className="w-4 h-4 text-primary fill-primary" />
+          {movie.vote_average.toFixed(1)}
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default MovieCard
