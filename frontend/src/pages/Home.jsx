6b9f8ec84946e4aac0451529e1c504359b8745fb
SK sanoyar ali <asksanoyar33@gmail.com>
2025-10-13 20:08:47 +0530

    added trailer section ui

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
 import FeaturedSection from '../components/featuredSection'
+import TrailerSection from '../components/TrailerSection'
 
 const Home = () => {
   return (
     <>
       <HeroSection />
       <FeaturedSection />
+      <TrailerSection />
     </>
   )
 }
