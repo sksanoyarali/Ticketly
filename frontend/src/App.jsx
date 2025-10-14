f74158e38e76a5de9562830c062860d6d33164c0
SK sanoyar ali <asksanoyar33@gmail.com>
2025-10-14 23:08:59 +0530

    added movieDeatail page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
         <Route path="/movies/:id" element={<MovieDetails />} />
         <Route path="/movies/:id/:date" element={<SeatLayout />} />
         <Route path="/my-bookings" element={<MyBookings />} />
-        <Route path="/favorite" element={<Favorite />} />
+        <Route path="/favorites" element={<Favorite />} />
       </Routes>
       {!isAdminRoute && <Footer />}
     </>
