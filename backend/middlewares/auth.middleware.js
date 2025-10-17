ba1f169079dc76115c0cc2e74eecaab023bd790b
SK sanoyar ali <asksanoyar33@gmail.com>
2025-10-17 23:56:52 +0530

    fix:bug fix at protect Admin middleware

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -2,8 +2,15 @@ import { clerkClient } from '@clerk/express'
 
 const protectAdmin = async (req, res, next) => {
   try {
-    const { userId } = req.auth()
+    const { userId } = req.auth
+    if (!userId) {
+      return res.status(401).json({
+        success: false,
+        message: 'Unauthorized — no valid token provided',
+      })
+    }
     const user = await clerkClient.users.getUser(userId)
+
     if (user.privateMetadata.role != 'admin') {
       return res.status(403).json({
         success: false,
