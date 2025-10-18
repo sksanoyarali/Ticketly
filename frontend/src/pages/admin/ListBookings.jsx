ee8f38a0f478854f2ef6b7c3d03a119ccd10e89a
SK sanoyar ali <asksanoyar33@gmail.com>
2025-10-18 21:07:54 +0530

    feat:make list-booking functions for admins

--- frontend/src/pages/admin/ListBookings.jsx
+++ frontend/src/pages/admin/ListBookings.jsx
@@ -3,20 +3,40 @@ import { dummyBookingData } from '../../assets/assets'
 import Loading from '../../components/Loading'
 import Title from '../../components/admin/Title'
 import { dateFormat } from '../../lib/dateFormat'
+import { useAppContext } from '../../context/AppContext'
+import toast from 'react-hot-toast'
 
 const ListBookings = () => {
+  const { axios, getToken, user } = useAppContext()
   const currency = import.meta.env.VITE_CURRENCY
 
   const [bookings, setBookings] = useState([])
   const [isLoading, setIsLoading] = useState(true)
 
   const getAllBookings = async () => {
-    setBookings(dummyBookingData)
+    try {
+      const { data } = await axios.get('/api/admin/all-bookings', {
+        headers: {
+          Authorization: `Bearer ${await getToken()}`,
+        },
+      })
+      if (data.success) {
+        setBookings(data.bookings)
+      } else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+      console.error(error)
+
+      toast.error('Error occured fetching bookings')
+    }
     setIsLoading(false)
   }
   useEffect(() => {
-    getAllBookings()
-  }, [])
+    if (user) {
+      getAllBookings()
+    }
+  }, [user])
   return !isLoading ? (
     <>
       <Title text1="List" text2="Bookings" />
