ce53019ac43c452f1766aaf9597cf141fb89042e
SK sanoyar ali <asksanoyar33@gmail.com>
2025-11-01 19:42:27 +0530

    added nodemailer email sender

---
+++ backend/configs/nodemailer.js
@@ -0,0 +1,23 @@
+import nodemailer from 'nodemailer'
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+})
+
+const sendEmail = async ({ to, subject, body }) => {
+  const response = await transporter.sendMail({
+    from: process.env.SENDER_EMAIL,
+    to: to,
+    subject: subject,
+    html: body, // HTML body
+  })
+  return response
+}
+
+export default sendEmail
