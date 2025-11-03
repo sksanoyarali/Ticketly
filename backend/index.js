e55753beb3985175ef6dae974ae0f7df0f0b264e
SK sanoyar ali <asksanoyar33@gmail.com>
2025-11-03 20:55:08 +0530

    addedfrontend url for cors

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,10 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: [
+      'http://localhost:5173',
+      'https://ticketly-backend-eight.vercel.app',
+    ],
     credentials: true,
   })
 )
